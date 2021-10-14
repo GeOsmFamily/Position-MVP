1ee5058b17c339c50d3a1f1d99fb63106ddfcd8e
SherlockHolmes2045 <lemovou@gmail.com>
2021-10-14 22:14:45 +0100

    fixed issue with form validation

--- position-admin-mvp/src/DemoPages/UserPages/LoginBoxed.vue
+++ position-admin-mvp/src/DemoPages/UserPages/LoginBoxed.vue
@@ -14,31 +14,55 @@
                     <span>Please sign in to your account below.</span>
                   </h4>
                 </div>
+                <b-form name="form" @submit.stop.prevent>
                 <b-form-group
                   id="exampleInputGroup1"
                   label-for="exampleInput1"
-                  description="We'll never share your email with anyone else."
                 >
                   <b-form-input
+                    v-model="email"
+                    :state="!submitted ? null: submitted && !$v.email.$invalid"
+                    v-model.trim="$v.email.$model"
+                    class="form-control"
+                    name="email"
                     id="exampleInput1"
                     type="email"
-                    required
                     placeholder="Enter email..."
                   >
                   </b-form-input>
+                  <b-form-invalid-feedback :state="!submitted ? null: submitted && $v.email.required">
+                    Field is required
+                  </b-form-invalid-feedback>
+                  <b-form-invalid-feedback :state="!submitted ? null: submitted && $v.email.email">
+                    This field must be an email
+                  </b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group id="exampleInputGroup2" label-for="exampleInput2">
                   <b-form-input
+                    v-model="$v.password.$model"
+                    :state="!submitted ? null : submitted && !$v.password.$invalid"
+                    class="form-control"
+                    name="password"
                     id="exampleInput2"
                     type="password"
                     required
                     placeholder="Enter password..."
                   >
                   </b-form-input>
+                  <b-form-invalid-feedback :state=" !submitted ? null : submitted && $v.password.required">
+                    Field is required
+                  </b-form-invalid-feedback>
+                  <b-form-invalid-feedback :state="!submitted ? null : submitted && $v.password.minLength">
+                    Password must have at least {{$v.password.$params.minLength.min}} letters.
+                  </b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-checkbox name="check" id="exampleCheck">
                   Keep me logged in
                 </b-form-checkbox>
+                  <div class="form-group">
+                    <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+                  </div>
+                </b-form>
                 <div class="divider" />
                 <h6 class="mb-0">
                   No account?
@@ -54,9 +78,10 @@
                   >
                 </div>
                 <div class="float-right">
-                  <b-button variant="primary" size="lg"
-                    >Login to Dashboard</b-button
-                  >
+                  <b-button variant="primary" size="lg" :disabled="loading" @click="handleLogin">
+                    <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+                    <span>Login to Dashboard</span>
+                  </b-button>
                 </div>
               </div>
             </div>
@@ -69,3 +94,69 @@
     </div>
   </div>
 </template>
+
+<script>
+import User from '../../models/user';
+import { required, minLength, email } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      user: new User('', ''),
+      loading: false,
+      submitted:false,
+      message: '',
+      email: '',
+      password:'',
+    };
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    }
+  },
+  created() {
+    if (this.loggedIn) {
+      this.$router.push('/profile');
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email,
+      minLength: minLength(4)
+    },
+    password: {
+      required,
+      minLength: minLength(4)
+    }
+  },
+  methods: {
+    handleLogin() {
+      this.loading = true;
+      this.submitted = true;
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        console.log("invalid");
+        this.loading = false;
+      } else {
+        if (this.user.username && this.user.password) {
+          this.$store.dispatch('auth/login', this.user).then(
+            () => {
+              this.$router.push('/profile');
+            },
+            error => {
+              this.loading = false;
+              this.message =
+                (error.response && error.response.data) ||
+                error.message ||
+                error.toString();
+            }
+          );
+        }
+      }
+    }
+  }
+};
+</script>
--- position-admin-mvp/src/main.js
+++ position-admin-mvp/src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import BootstrapVue from "bootstrap-vue";
-import VeeValidate from "vee-validate";
+import Vuelidate from 'vuelidate';
 import Default from "./Layout/Wrappers/baseLayout.vue";
 import Pages from "./Layout/Wrappers/pagesLayout.vue";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-Vue.use(VeeValidate);
+Vue.use(Vuelidate);
 Vue.component("default-layout", Default);
 Vue.component("userpages-layout", Pages);
 new Vue({
