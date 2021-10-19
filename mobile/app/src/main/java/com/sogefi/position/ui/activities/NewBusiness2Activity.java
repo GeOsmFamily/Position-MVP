f684f07013ccf4b44371145ab7460f8e3cc9b0ab
BorisGautier <tchoukouahaboris@outlook.fr>
2021-10-19 16:57:55 +0100

    Add View create new Business in mobile app

---
+++ mobile/app/src/main/java/com/sogefi/position/ui/activities/NewBusiness2Activity.java
@@ -0,0 +1,14 @@
+package com.sogefi.position.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewBusiness2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_business2);
+    }
+}
