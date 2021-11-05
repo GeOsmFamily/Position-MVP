835b0f1b87ffdf08a9707533ef9d022e162df8fc
SherlockHolmes2045 <lemovou@gmail.com>
2021-11-05 17:33:14 +0100

    imported vue datatables

--- position-admin-mvp/src/main.js
+++ position-admin-mvp/src/main.js
@@ -3,28 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import BootstrapVue from "bootstrap-vue";
-import Vuelidate from 'vuelidate';
+import Vuelidate from "vuelidate";
 import Default from "./Layout/Wrappers/baseLayout.vue";
 import Pages from "./Layout/Wrappers/pagesLayout.vue";
 import api from "@/service/api";
+import VueDataTables from "vue-data-tables";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = api;
 api.defaults.timeout = 10000;
 api.interceptors.request.use(
-  config => {
+  (config) => {
     const token = localStorage.getItem("access_token");
     if (token) {
       config.headers.common["Authorization"] = token;
     }
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
 api.interceptors.response.use(
-  response => {
+  (response) => {
     console.log("interceptor");
     if (response.status === 200 || response.status === 201) {
       return Promise.resolve(response);
@@ -32,11 +33,10 @@ api.interceptors.response.use(
       return Promise.reject(response);
     }
   },
-  error => {
+  (error) => {
     if (error.response.status) {
       switch (error.response.status) {
         case 400:
-
           //do something
           break;
 
@@ -46,19 +46,19 @@ api.interceptors.response.use(
         case 403:
           router.replace({
             path: "/login",
-            query: { redirect: router.currentRoute.fullPath }
+            query: { redirect: router.currentRoute.fullPath },
           });
           break;
         case 404:
-          alert('page not exist');
+          alert("page not exist");
           break;
         case 502:
           setTimeout(() => {
             router.replace({
               path: "/login",
               query: {
-                redirect: router.currentRoute.fullPath
-              }
+                redirect: router.currentRoute.fullPath,
+              },
             });
           }, 1000);
       }
@@ -68,6 +68,7 @@ api.interceptors.response.use(
 );
 Vue.use(BootstrapVue);
 Vue.use(Vuelidate);
+Vue.use(VueDataTables);
 Vue.component("default-layout", Default);
 Vue.component("userpages-layout", Pages);
 new Vue({
