3eaf39c1279067e338837958f7eafbd28f0068d9
BorisGautier <tchoukouahaboris@outlook.fr>
2021-11-16 14:30:03 +0100

    update api service

---
+++ mobile/app/src/main/java/com/sogefi/position/models/data/DataUser.java
@@ -0,0 +1,31 @@
+package com.sogefi.position.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class DataUser {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+    @SerializedName("user")
+    @Expose
+    private User user;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
