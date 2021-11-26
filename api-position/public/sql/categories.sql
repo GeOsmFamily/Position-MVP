29ed6858901a5605d5cca2de8b7852be46d3b044
BorisGautier <tchoukouahaboris@outlook.fr>
2021-11-26 15:27:22 +0100

    update categorie

--- api-position/public/sql/categories.sql
+++ api-position/public/sql/categories.sql
@@ -1,41 +1,41 @@
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (1, 'Achats', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (2, 'Administrations', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (3, 'Agriculture', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (4, 'Alimentation', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (5, 'Automobile, Moto, Engins', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (6, 'Banques, finances et assurances', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (7, 'Batiments & Constructions', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (8, 'Bien-être', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (9, 'Commerce - Import & Export', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (
     10,
@@ -43,31 +43,31 @@ VALUES
     NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP
   );
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (11, 'Eau', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (12, 'Education & Formation', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (13, 'Energie', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (14, 'Hydrocarbures, Pétroliers, Forages', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (15, 'Immobilier', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (16, 'Industries', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (
     17,
@@ -75,23 +75,23 @@ VALUES
     NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP
   );
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (18, 'Justice', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (19, 'Loisirs', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (20, 'Restos, bars', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (21, 'Santé & Médecine', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (
     22,
@@ -99,22 +99,22 @@ VALUES
     NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP
   );
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (23, 'Télécommunication', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (24, 'Textiles & Prêt à Porter', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (25, 'Tourisme', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (26, 'Transports', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
   INSERT INTO
-  categories(id, nom, logo_url,created_at,updated_at)
+  categories(id, nom, logoUrl,created_at,updated_at)
 VALUES
   (27, 'Autres', NULL,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
