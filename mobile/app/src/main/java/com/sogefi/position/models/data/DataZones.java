e344ca869d5f2d22c7846bc0a9dc2e636cdbe70a
BorisGautier <tchoukouahaboris@outlook.fr>
2021-12-07 17:36:26 +0100

    Final update app mobile

---
+++ mobile/app/src/main/java/com/sogefi/position/models/data/DataZones.java
@@ -0,0 +1,4 @@
+package com.sogefi.position.models.data;
+
+public class DataZones {
+}
---
+++ mobile/app/src/main/java/com/sogefi/position/ui/activities/DetailsBusinessActivity.java
@@ -0,0 +1,14 @@
+package com.sogefi.position.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_business);
+    }
+}
