c56a866bed3c99925fc39758cd044e96be32c54b
youngdevps <jordiibayo@gmail.com>
2021-12-07 16:44:51 +0100

    add sql port

--- services/.cmd
+++ services/.cmd
@@ -1,4 +1,5 @@
 docker build -t position-container:v0.1 .
 docker-compose up --build -d
 docker network connect position-network position-api
+docker network connect position-network api-position-mysql
 docker network inspect position-network
