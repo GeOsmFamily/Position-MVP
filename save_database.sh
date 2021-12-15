1d9e57cefd63a62e30b5357e240969603b94e4ac
BorisGautier <tchoukouahaboris@outlook.fr>
2021-12-15 06:28:20 +0100

    add script to save database

---
+++ save_database.sh
@@ -0,0 +1,34 @@
+# Backup storage directory 
+backupfolder=/var/backups
+# Notification email address 
+recipient_email=<bt@geo.sm>
+# MySQL user
+user=<root>
+# MySQL password
+password=<positionApi2021>
+#MySQL host
+host=<localhost:9501>
+# Number of days to store the backup 
+keep_day=30 
+sqlfile=$backupfolder/all-database-$(date +%d-%m-%Y_%H-%M-%S).sql
+zipfile=$backupfolder/all-database-$(date +%d-%m-%Y_%H-%M-%S).zip 
+# Create a backup 
+sudo mysqldump -h $host -u $user -p$password --all-databases > $sqlfile 
+if [ $? == 0 ]; then
+  echo 'Sql dump created' 
+else
+  echo 'mysqldump return non-zero code' | mailx -s 'No backup was created!' $recipient_email  
+  exit 
+fi 
+# Compress backup 
+zip $zipfile $sqlfile 
+if [ $? == 0 ]; then
+  echo 'The backup was successfully compressed' 
+else
+  echo 'Error compressing backup' | mailx -s 'Backup was not created!' $recipient_email 
+  exit 
+fi 
+rm $sqlfile 
+echo $zipfile | mailx -s 'Backup was successfully created' $recipient_email 
+# Delete old backups 
+find $backupfolder -mtime +$keep_day -delete
