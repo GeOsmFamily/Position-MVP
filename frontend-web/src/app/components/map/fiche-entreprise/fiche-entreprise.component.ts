73d7975312baf6cd62711f56f27f7e4e35cfd40b
BorisGautier <tchoukouahaboris@outlook.fr>
2021-12-15 13:52:52 +0100

    fix bugs frontend web

--- frontend-web/src/app/components/map/fiche-entreprise/fiche-entreprise.component.ts
+++ frontend-web/src/app/components/map/fiche-entreprise/fiche-entreprise.component.ts
@@ -5,7 +5,12 @@ import * as $ from 'jquery';
 import { Feature, Point } from 'src/app/modules/ol';
 import { DeviceDetectionService } from 'src/app/services/device-detection.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ActivatedRoute, Router, NavigationStart, NavigationEnd } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  NavigationStart,
+  NavigationEnd,
+} from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 import { filter, map } from 'rxjs/operators';
 
@@ -17,69 +22,65 @@ import { filter, map } from 'rxjs/operators';
 export class FicheEntrepriseComponent implements OnInit {
   featurePoint: Feature<Point> | undefined;
   url_image = environment.url_image;
- // url_position = environment.url_frontend;
+  // url_position = environment.url_frontend;
   url_demo = environment.url_demo;
   images = [];
   constructor(
-      private meta: Meta,
+    private meta: Meta,
     private router: Router,
     private modalService: NgbModal,
     private activatedRoute: ActivatedRoute,
     private componentHelper: ComponentHelper
-  ) {
-    }
+  ) {}
 
   ngOnInit(): void {}
 
-
-
-
   private setupRouting() {
-    this.router.events.pipe(
-      filter((event: any) => event instanceof NavigationEnd),
-      map(() => this.activatedRoute),
-      map(route => {
-        while (route.firstChild) {
-          route = route.firstChild;
-        }
-        return route;
-      }),
-      filter(route => route.outlet === 'primary')
-    ).subscribe((route: ActivatedRoute) => {
-      const seo = route.snapshot.data['seo'];
-      this.meta.updateTag({
-        property: 'og:title',
-        content: this.featurePoint?.get('textLabel'),
-      });
-      this.meta.updateTag({
-        property: 'og:description',
-        content:"Retrouvez mon business sur la plateforme Position"
+    this.router.events
+      .pipe(
+        filter((event: any) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary')
+      )
+      .subscribe((route: ActivatedRoute) => {
+        const seo = route.snapshot.data['seo'];
+        this.meta.updateTag({
+          property: 'og:title',
+          content: this.featurePoint?.get('textLabel'),
+        });
+        this.meta.updateTag({
+          property: 'og:description',
+          content: 'Retrouvez mon business sur la plateforme Position',
           //this.featurePoint?.get('nomCategorie') +',' + this.featurePoint?.get('nomSousCategorie'),
-      });
-      this.meta.updateTag({
-        property: 'og:image',
-        content:  environment.url_image+ this.featurePoint?.get('cover'),
-      });
-      this.meta.updateTag({
-        property: 'og:url',
-        content:
-          environment.url_demo +
-          'home?etablissements=' +
-          this.featurePoint?.get('id') +
-          ',16',
-      });
+        });
+        this.meta.updateTag({
+          property: 'og:image',
+          content: environment.url_image + this.featurePoint?.get('cover'),
+        });
+        this.meta.updateTag({
+          property: 'og:url',
+          content:
+            environment.url_demo +
+            'home?etablissements=' +
+            this.featurePoint?.get('id') +
+            ',16',
+        });
 
-      // set your meta tags & title here
-
-    });
+        // set your meta tags & title here
+      });
   }
 
   shareLink() {
-
-    this.setupRouting()
+    this.setupRouting();
     var url_share =
-      environment.url_demo+
-      'home/?etablissements=' +
+      environment.url_demo +
+      'home?etablissements=' +
       this.featurePoint?.get('id') +
       ',16';
     this.componentHelper.openSocialShare(url_share, 7);
@@ -94,12 +95,10 @@ export class FicheEntrepriseComponent implements OnInit {
 
     this.images = this.featurePoint?.get('imagesCarousel');
 
-
     $('app-fiche-entreprise').css('left', '0px');
   }
 
   close() {
-
     $('app-fiche-entreprise').css('left', '-350px');
   }
   alert1() {
@@ -112,8 +111,6 @@ export class FicheEntrepriseComponent implements OnInit {
     modalRef.componentInstance.my_modal_title =this.featurePoint?.get("telephones")[0].principal;
     modalRef.componentInstance.my_modal_content = 'I am your content';
     */
-   // alert(this.featurePoint?.get('telephones')[0].principal);
-
-
+    // alert(this.featurePoint?.get('telephones')[0].principal);
   }
 }
