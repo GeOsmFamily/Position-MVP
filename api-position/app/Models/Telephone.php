ecee5a3926e028b2d7ba0ec3dd2a96c85c9a8ad1
BorisGautier <tchoukouahaboris@outlook.fr>
2021-12-16 08:55:40 +0100

    update api service with soft delete

--- api-position/app/Models/Telephone.php
+++ api-position/app/Models/Telephone.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * App\Models\Telephone
@@ -30,7 +31,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Telephone extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         "idEtablissement", "numero", "whatsapp", "principal"
